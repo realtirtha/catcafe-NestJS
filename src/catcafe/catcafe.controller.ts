91a481b87767a67b7a944e7ffa835f6ab20664b3
Tirth <tirthacodes@gmail.com>
2023-06-28 15:15:46 +0545

    catcafe controller modified

--- src/catcafe/catcafe.controller.ts
+++ src/catcafe/catcafe.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CatcafeService } from './catcafe.service';
+import { CreateCatCafeDto } from './dtos/create-catcafe.dto';
 
 @Controller('catcafe')
-export class CatcafeController {}
+export class CatcafeController {
+    catcafeService: CatcafeService;
+
+    constructor(){
+        this.catcafeService= new CatcafeService;
+    }
+
+    @Get()
+    AllFoodOrder(){
+        return this.catcafeService.AllFoodOrder();
+    }
+
+    @Get('/:id')
+    OrderById(@Param('id') id:string){
+        return this.catcafeService.OrderById(id);
+    }
+
+    @Post()
+    CreateFoodOrder(@Body() body: CreateCatCafeDto){
+        return this.catcafeService.CreateFoodOrder(body);
+    }
+}
