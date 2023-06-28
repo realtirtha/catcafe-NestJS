00e409b88e668b3d6fdcc46ade67ed0339030520
Tirth <tirthacodes@gmail.com>
2023-06-28 15:22:17 +0545

    main.ts modified

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { CatcafeModule } from './catcafe/catcafe.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(CatcafeModule);
   await app.listen(3000);
 }
 bootstrap();
