2c62404604b03b03ed6f9e41d20aa96417efe58d
Tirth <tirthacodes@gmail.com>
2023-06-28 15:04:32 +0545

    modified service

--- src/catcafe/catcafe.service.ts
+++ src/catcafe/catcafe.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { CatCafeRepository } from './catcafe.repository';
+import { CreateCatCafeDto } from './dtos/create-catcafe.dto';
 
 @Injectable()
-export class CatcafeService {}
+export class CatcafeService {
+
+    catcafeRepo:CatCafeRepository;
+
+    constructor(){
+        this.catcafeRepo = new CatCafeRepository();
+    }
+
+    AllFoodOrder(){
+        return this.catcafeRepo.AllFoodOrder();
+    }
+
+    OrderById(id:string){
+        return this.catcafeRepo.OrderById(id);
+    }
+
+    CreateFoodOrder(dto: CreateCatCafeDto){
+        const {customer_name, customer_id, customer_order, customer_orderQ} = dto;
+        return this.catcafeRepo.CreateFoodOrder(customer_name, customer_id, customer_order, customer_orderQ);
+    }
+
+}
